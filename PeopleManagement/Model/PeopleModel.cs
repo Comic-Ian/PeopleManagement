c2613af2375619cb5f03ab7f2ba08a1561e1b286
Haojun.Zhao <Haojun.Zhao@icloud.com>
2018-04-17 17:28:57 +0800

    feature:添加一个Demo

---
+++ PeopleManagement/Model/PeopleModel.cs
@@ -0,0 +1,23 @@
+﻿namespace PeopleManagement.Model
+{
+    /// <summary>
+    /// 人物模型
+    /// </summary>
+    public class PeopleModel
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string PeopleName { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public SexEnum Sex { get; set; }
+
+    }
+}
---
+++ PeopleManagement/Model/SexEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace PeopleManagement.Model
+{
+    public enum SexEnum
+    {
+
+        女 = 0,
+        男 = 1
+
+    }
+}
